1674e07b96bfb45f6382ab6e4650167e86c6ad9d
Black Smith-JD-40 <ol.channel.t@gmail.com>
2023-02-24 02:07:57 +0400

    Revising & Checking commit

--- api/src/main/java/Main.java
+++ api/src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("РАботает!!!!!!");
+        System.out.println("ПРОВЕРКА!!!!!!");
         MyService myService = new MyService();
         System.out.println(myService.getMyEntity());
         System.out.println(myService.setMyEntity(new MyEntity("second")));
